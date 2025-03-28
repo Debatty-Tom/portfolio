06365dc02ec0a46eef19259c784224cdc1fd0fb2
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-03-28 14:40:37 +0100

    add about and contact template

---
+++ wp-content/themes/portfolio/template-contact.php
@@ -0,0 +1,26 @@
+<?php /* Template Name: Page "Contact" */ ?>
+
+<?php get_header(); ?>
+<aside>
+    <h2><?= __hepl('Contactez-moi') ?></h2>
+</aside>
+<?php
+// On ouvre "la boucle" (The Loop), la structure de contrôle
+// de contenu propre à Wordpress:
+if(have_posts()): while(have_posts()): the_post(); ?>
+
+    <div><?= get_the_content(); ?></div>
+
+<?php
+    // On ferme "la boucle" (The Loop):
+endwhile; else: ?>
+    <p><?= __hepl('La page est vide') ?>.</p>
+<?php endif; ?>
+<?php get_footer(); ?>
+
+
+
+
+
+
+
