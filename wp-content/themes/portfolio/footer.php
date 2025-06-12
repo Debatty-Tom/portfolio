ecf85e82d0f04373a259803ae0c2b6412d06e783
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-06-12 17:57:42 +0200

    Update footer.php

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,10 +1,10 @@
-<?php if (!is_front_page()):
+<?php if (!is_front_page() && !is_page_template('template-privacy.php') && !is_404()):
     get_template_part('parts/section', 'explore-more');
 endif; ?>
 </main>
 <footer class="footer">
-    <p>© 2024 <?= get_bloginfo('name'); ?> - Tous droits réservés.</p>
-    <a href="" class="footer__link">Mentions légales</a>
+    <p>© 2024 <?= get_bloginfo('name'); ?> - <?= __hepl('Tous droits réservés') ?>.</p>
+    <a href="<?= dw_get_permalink('mention-legales') ?>" class="footer__link"><?= __hepl('Mentions légales') ?></a>
 </footer>
 </body>
 </html>
