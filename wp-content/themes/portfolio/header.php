caa33a1877d2077e7131e40f9f0c65ed3e024d4e
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-06-12 11:44:21 +0200

    fix header

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -10,63 +10,85 @@
 </head>
 <body>
 <header class="header" role="banner">
-    <a class="header__title" href="<?= home_url() ?>" title="Vers la page d'accueil"><?= get_bloginfo('name') ?></a>
-    <h1 class="page__title"><?= dw_get_page_title() ?></h1>
-    <nav class="nav">
-        <h2 class="sro"><?= __hepl('Navigation principale') ?></h2>
-        <label class="sro" for="burger">Burger menu</label>
-        <input type="checkbox" name="burger" id="burger">
-        <div class="burger__wrapper">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <ul class="nav__container">
-            <li class="nav__sublist__container--projects">
-                <ul class="nav__list nav__list--projects">
-                    <?php
-                    $projectIndex = 0;
-                    foreach (dw_get_navigation_links('header') as $link):
-                        if (str_contains($link->href, 'projets')):
-                            $projectIndex++;
-                            ?>
-                            <li class="nav__item nav__item--project nav__item--project--<?= $projectIndex ?>">
-                                <a href="<?= $link->href; ?>" class="nav__link" title="<?= __('lien vers le projet nommé ' . $link->label) ?>">
-                                    <?php
-                                    $post_id = url_to_postid($link->href);
-                                    if ($post_id && get_post_type($post_id) === 'project'): ?>
-                                        <figure class="project__figure">
-                                            <?= get_the_post_thumbnail($post_id, 'naviguation', ['class' => 'project__figure__img']) ?>
-                                            <figcaption class="project__label"><?= $link->label; ?></figcaption>
-                                        </figure>
-                                    <?php endif; ?>
-                                </a>
-                            </li>
-                        <?php endif;
-                    endforeach; ?>
-                </ul>
-            </li>
-            <li class="nav__sublist__container">
-                <ul class="nav__list nav__list--links">
-                    <?php
-                    $linkIndex = 0;
-                    foreach (dw_get_navigation_links('header') as $link):
-                        if (!str_contains($link->href, 'projets')):
-                            ?>
-                            <li class="nav__item nav__item--link">
-                                <a href="<?= $link->href; ?>" class="nav__link"><?= $link->label; ?></a>
-                            </li>
-                        <?php endif;
-                    endforeach; ?>
-                    <?php foreach (pll_the_languages(['raw' => true]) as $lang): ?>
-                        <li class="languages__item<?= $lang['current_lang'] ? ' languages__item--current' : '' ?>">
-                            <a href="<?= $lang['url'] ?>" lang="<?= $lang['locale'] ?>" hreflang="<?= $lang['locale'] ?>"
-                               class="nav__link"><?= $lang['locale'] ?></a>
+    <h1 class="sro"><?= dw_get_page_title() ?></h1>
+    <div class="header__container">
+        <nav class="nav">
+            <h2 class="sro"><?= __hepl('Navigation principale') ?></h2>
+            <a class="header__title" href="<?= home_url() ?>"
+               title="Vers la page d'accueil"><?= get_bloginfo('name') ?></a>
+            <label class="sro" for="burger">Burger menu</label>
+            <input type="checkbox" name="burger" id="burger">
+            <div class="burger__wrapper">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul class="nav__container">
+                <?php foreach (dw_get_navigation_links('header') as $link):
+                    if (!str_contains($link->href, 'projets')): ?>
+                        <li class="nav__item">
+                            <a href="<?= $link->href; ?>" class="nav__link underline"><?= $link->label; ?></a>
                         </li>
-                    <?php endforeach; ?>
-                </ul>
-            </li>
+                    <?php endif;
+                endforeach; ?>
+                <?php foreach (pll_the_languages(['raw' => true]) as $lang): ?>
+                    <li class="languages__item<?= $lang['current_lang'] ? ' languages__item--current' : '' ?>">
+                        <a href="<?= $lang['url'] ?>" lang="<?= $lang['locale'] ?>"
+                           hreflang="<?= $lang['locale'] ?>"
+                           class="nav__link underline"><?= $lang['locale'] ?></a>
+                    </li>
+                <?php endforeach; ?>
+                <li class="nav__item nav__sublist__container">
+                    <p class="nav__sublist__title">
+                        <?php __hepl('Mes projets') ?>
+                    </p>
+                    <ul class="nav__list">
+                        <?php
+                        foreach (dw_get_navigation_links('header') as $link):
+                            if (str_contains($link->href, 'projets')):
+                                ?>
+                                <li class="nav__item underline">
+                                    <a href="<?= $link->href; ?>" class="nav__link"
+                                       title="<?= __('lien vers le projet nommé ' . $link->label) ?>">
+                                    </a>
+                                </li>
+                            <?php endif;
+                        endforeach; ?>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <section class="project__container">
+        <h2 class="page__title"><?= dw_get_page_title() ?></h2>
+        <ul class="project__list">
+            <?php
+            $projects = new WP_Query([
+                'post_type' => 'project',
+                'posts_per_page' => -1,
+            ]);
+
+            $projectIndex = 0;
+
+            if ($projects->have_posts()):
+                while ($projects->have_posts()): $projects->the_post();
+                    $projectIndex++;
+                    $title = get_the_title();
+                    $permalink = get_permalink();
+                    ?>
+                    <li class="project__item project__item--<?= $projectIndex ?>">
+                        <a href="<?= esc_url($permalink) ?>" class="project__link"
+                           title="<?= esc_attr(__('Lien vers le projet nommé ' . $title)) ?>">
+                            <figure class="project__figure">
+                                <?= get_the_post_thumbnail(get_the_ID(), 'naviguation', ['class' => 'project__figure__img']) ?>
+                                <figcaption class="project__label"><?= esc_html($title) ?></figcaption>
+                            </figure>
+                        </a>
+                    </li>
+                <?php endwhile;
+                wp_reset_postdata();
+            endif;
+            ?>
         </ul>
-    </nav>
+    </section>
 </header>
-<main>
