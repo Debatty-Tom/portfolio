7b0e3a9332493c02c1e47ad8b60053905a60f562
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-06-12 11:44:58 +0200

    fix skills section

--- wp-content/themes/portfolio/parts/section-skills-svg-used.php
+++ wp-content/themes/portfolio/parts/section-skills-svg-used.php
@@ -1,6 +1,10 @@
 <section class="skills-wall-section">
     <h2 class="section__title"><?= __hepl('Mes compétences') ?></h2>
     <?php if (have_rows('logos_repeater')): ?>
+    <?php
+        $positions = dwGenerateUniqueGridPositions(count(get_field('logos_repeater')));
+        $index = 0;
+        ?>
         <ul class="skills-wall">
             <?php while (have_rows('logos_repeater')): the_row();
                 $selected_svg = get_sub_field('logo_select');
@@ -14,19 +18,24 @@
                     if (file_exists($svg_path)) {
                         $svg_content = file_get_contents($svg_path);
                     } else {
-                        $svg_content = '<p>SVG introuvable</p>';
+                        continue;
                     }
                 } else {
                     $svg_content = '<p>Aucune sélection</p>';
                     $svg_label = 'Inconnu';
                 }
                 ?>
-                <li class="skills-wall__item">
-                    <div class="svg-container" title="<?= 'Il s’agit du logo de '.$svg_label ?>">
+                <li class="skills-wall__item <?= $positions[$index++] ?>">
+                    <div class="svg-container" title="<?= 'Il s’agit du logo de ' . $svg_label ?>">
                         <desc>
                             <?php get_field('logo_description'); ?>
                         </desc>
-                        <?php echo $svg_content; ?>
+                        <figure>
+                            <?php echo $svg_content; ?>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
                     </div>
                 </li>
             <?php endwhile; ?>
