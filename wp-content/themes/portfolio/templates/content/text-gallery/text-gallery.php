28da0a0d020d1844b023fabb28644a7871498dbb
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-06-13 18:22:20 +0200

    fix empty gallery condition

--- wp-content/themes/portfolio/templates/content/text-gallery/text-gallery.php
+++ wp-content/themes/portfolio/templates/content/text-gallery/text-gallery.php
@@ -2,7 +2,7 @@
 <?php $media_position = get_sub_field('gallery_position') ?>
 
 <div class="text-media__position text-media__position--<?= $media_position ?> gallery">
-    <?php if (isset($gallery) && $gallery !== []): ?>
+    <?php if (isset($gallery) && $gallery !== false): ?>
         <div class="gallery__container text-media__image" title="<?= __hepl('Voir la galerie') ?>">
             <?php foreach ($gallery as $image): ?>
                 <a class="gallery__link" href="<?= wp_get_attachment_image_url($image['id'], 'full') ?>"
